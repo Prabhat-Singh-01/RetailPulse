95cc97bc5e14e70306a2dc8d3d6d6fb99424566e
Prabhat Singh <prabhatsingh.connect.01@gmail.com>
2026-03-30 11:17:59 +0530

    Delete Category_5 Payment Analysis.sql

--- Category_5 Payment Analysis.sql
+++
@@ -1,8 +0,0 @@
--- Q15. Most Popular Payment Methods
-SELECT 
-    payment_type,
-    COUNT(*) AS total_transactions,
-    ROUND(SUM(payment_value)::numeric, 2) AS total_value
-FROM order_payments
-GROUP BY payment_type
-ORDER BY total_transactions DESC;
